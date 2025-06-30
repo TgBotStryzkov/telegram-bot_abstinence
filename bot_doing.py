a43804a77ae1d81b79941937e5164779f8312747
TgBotStryzkov <mihailstryzkov@gmail.com>
2025-06-30 22:15:46 +0300

    Update bot_doing.py

--- bot_doing.py
+++ bot_doing.py
@@ -1,15 +1,13 @@
 import asyncio
-from telegram.ext import Application, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 async def start(update, context):
-    await update.message.reply_text("Привет! Я работаю!")
+    await update.message.reply_text("Привет! Я антислив-бот.")
 
-def main():
-    app = Application.builder().token("ТВОЙ_ТОКЕН").build()
-
+async def main():
+    app = ApplicationBuilder().token("8164293392:AAEJU4G3OLHydRiv6pmh9RboxrK50_X7TPc").build()
     app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
 
-    app.run_polling()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
